4f1b9f4678402513bada6893e69bac9885a0e4b9
Maryam Mehzabin Zahin <zahinewu@gmail.com>
2022-01-10 23:57:54 +0600

    add feature

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ new Vue({
     el:"#app",
     data:
     {
+        new_item:"",
         todos:["reading","shopping"]
+    },
+    methods: {
+        addTodo()
+        {
+            this.todos.push(this.new_item)
+            this.new_item=""
+        }
     }
+    
 
 });
