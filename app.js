b54116bc3ef67df50be0a9757debfa758f96e471
Maryam Mehzabin Zahin <zahinewu@gmail.com>
2022-01-11 22:34:51 +0600

    comments added in js file

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-new Vue({
+new Vue({ //creating vue instance
     el:"#app",
     data:
     {
@@ -8,61 +8,54 @@ new Vue({
         isActive:false,
         selectedIndex:null,
         new_item:"",
-        todos:["reading","shopping"],
-        completed:[]
+        todos:["reading a book","dress shopping", "call a cousin", "watering the plants", "giving house keys to Maisha"],//list for active todos
+        completed:[]//list for completed tasks
     },
     methods: {
         addTodo()
         {
         
-            if(this.todo)
+            if(this.todo)//if the user add someting other than empty string, todo gets added to the todos list.
             {
             this.todos.push(this.todo)
             this.todo=""
             }
-            else
+            else//if user tries to add empty string
             {
                 window.alert("Please, write something")
-            }
-            
-            
-        },
+            }       
+         },
 
         editTodo(index,todo)
         {
-           
             this.todo=todo
-            
             this.selectedIndex=index
-            this.isEditing=true
-            this.isActive=true
-           
-           
+            this.isEditing=true//isEditing is turned true so the update is enabled rather than 'ADD'
+            this.isActive=true         
         },
 
         updateTodo()
-        {
-           
+        {         
             if(this.todo)
             {
-                this.todos.splice(this.selectedIndex,1,this.todo)
+                this.todos.splice(this.selectedIndex,1,this.todo) //deleting 1 element at the selectedIndex
                 this.isEditing=false
-                this.isActive=false
+                this.isActive=false //it is turned false so that all other buttons are disabled while updating
                 this.todo=""
             }
             else
             {
-                window.alert("Please, write something")
+                window.alert("Please, write something")//if user tries to add empty string
             }
             
         },
 
         deleteTodo(index)
         {
-            this.todos.splice(index,1)
+            this.todos.splice(index,1)//deleting 1 element at the selectedIndex
         },
 
-        goBack()
+        goBack()//when user tries to skip the update
         {
             this.isEditing=false
             this.isActive=false
@@ -72,19 +65,16 @@ new Vue({
         createCompleted(index,todo)
         {
             this.todo=todo
-            
             this.index=index
-            this.completed.push(this.todo)
-            this.todos.splice(this.index,1)
-            this.todo=""
-            
+            this.completed.push(this.todo) //creating completed list 
+            this.todos.splice(this.index,1)//deleting the completed todos from the active todos
+            this.todo=""           
         },
 
         deleteAll()
         {
-                for(var i=this.completed.length-1;i>=0;i--)
+                for(var i=this.completed.length-1;i>=0;i--)//deleting completed todos one by one by using their index values
                 {
-
                 this.completed.splice(i,1)
                 }
         }
